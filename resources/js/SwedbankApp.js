d60bed8fa9be2f8170c185c46e4a16b5b345f25e
Kęstutis <k.tautvydas@gmail.com>
2015-11-12 00:54:12 +0200

    animation

--- resources/js/SwedbankApp.js
+++ resources/js/SwedbankApp.js
@@ -1,23 +1,36 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    // counter starts at 0
+    Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    Template.hello.helpers({
+        counter: function () {
+            return Session.get('counter');
+        }
+    });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    Template.hello.events({
+        'click #button': function () {
+            $('#zzz').addClass("animated bounceInLeft");
+            // increment the counter when button is clicked
+            Session.set('counter', Session.get('counter') + 1);
+        }
+    });
+    Template.points_wrapper.events({
+        'click #slide-trigger': function(event) {
+            console.log(event);
+            if ($(event.currentTarget.parentNode).hasClass('expanded')) {
+                $(event.currentTarget.parentNode).animate({height: "182px"}, 500);
+                $(event.currentTarget.parentNode).removeClass('expanded');
+            } else {
+                $(event.currentTarget.parentNode).animate({height: "422px"}, 500);
+                $(event.currentTarget.parentNode).addClass('expanded');
+            }
+        }
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }

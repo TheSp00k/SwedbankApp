5df3c1b163848ccc15208848b3f13577ee04e785
Kęstutis <k.tautvydas@gmail.com>
2015-11-26 07:29:53 +0200

    update

--- client/shared/events.js
+++ client/shared/events.js
@@ -11,3 +11,58 @@ Template.points_explain.events({
         }
     }
 });
+
+Template.newQuestion.events({
+    'click #formSubmit': function(event) {
+
+        //http://hackersome.com/p/medihack/meteor-autoform
+        //https://www.google.lt/search?q=meteor+non-collection+forms&oq=meteor+non-collection+forms&aqs=chrome..69i57.10290j0j7&sourceid=chrome&es_sm=93&ie=UTF-8
+        //http://stackoverflow.com/questions/29890608/get-autoform-non-collection-forms-method-return-value
+        //https://github.com/aldeed/meteor-autoform#callbackshooks
+
+        console.log(event.target);
+        //var text = event.target.text.value;
+        //
+        //// Insert a task into the collection
+        //Tasks.insert({
+        //    text: text,
+        //    createdAt: new Date() // current time
+        //});
+        //
+        //// Clear form
+        //event.target.text.value = "";
+
+        var quizFields = {
+            legend: {
+                type: String,
+                //label: 'Title',
+                autoform: {
+                    value: 'ka tu'
+                }
+            },
+            title: {
+                type: String,
+                label: 'Titleee'
+            },
+            _id: {
+                type: String,
+                optional: true,
+                autoform: {
+                    omit: true
+                }
+            },
+            createdAt: {
+                type: Date,
+                optional: true,
+                autoform: {
+                    omit: true
+                }
+            }
+        };
+
+
+
+        QuizSchema = new SimpleSchema(quizFields);
+        Quiz.attachSchema(QuizSchema);
+    }
+});

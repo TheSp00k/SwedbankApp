90484444f7628116267361d0e32d332c735abaaf
Kęstutis <k.tautvydas@gmail.com>
2015-11-30 00:03:42 +0200

    update

--- client/questions/helpers.js
+++ client/questions/helpers.js
@@ -1,21 +1,78 @@
-
 Template.question.helpers({
-    getQuizFormSchema: function() {
-        return Schema.quizSchema;
+    getQuizFormSchema: function () {
+        return Schema.baseQuestion;
     },
-    getVarFromSession: function() {
-        return Session.get('result');
+    getVarFromSession: function () {
+        return Session.get('answer');
+    },
+    getQuestionsCount: function () {
+        //return Session.get('questions');
+    },
+    nextButton: "Toliau",
+    steps: function () {
+        return [
+            {
+                id: 'base',
+                //title: 'Information',
+                schema: Schema.baseQuestion,
+                data: function(data) {
+                    console.log('qqqq');
+                }
+                //onSubmit: function (data) {
+                //    if (data.answer == 1) {
+                //        user_type = 'points_user';
+                //    } else {
+                //        user_type = 'non-points_user';
+                //    }
+                //    Session.set('user_type', user_type);
+                //}
+                //onSubmit: function(data, wizard) {
+                //    console.log(wizard);
+                //    if (data.answer == 1) {
+                //        user_type = 'points_user';
+                //    } else {
+                //        user_type = 'non-points_user';
+                //    }
+                //    Session.set('user_type', user_type);
+                //}
+            },
+            {
+                id: 'question1',
+                //title: 'Confirm',
+                //schema: Schema.question1,
+                schema: function() {
+                    console.log('asdsadas');
+                    console.log(Session.get('user_type'));
+                    if (Session.get('user_type') == 'points_user') {
+                        return Schema.question1;
+                    } else {
+                        return Schema.question2;
+                    }
+                },
+                onSubmit: function (data, wizard) {
+                    if (data.answer == 1) {
+                        user_type = 'points_user';
+                    } else {
+                        user_type = 'non-points_user';
+                    }
+                    Session.set('user_type', user_type);
+                }
+            }
+        ]
+    },
+    nextButton: {
+        label: 'qweqwe',
+        id: 'qqq'
     }
 });
-
 
 AutoForm.hooks({
     'quizForm': {
         after: {
-            method: function(error, result) {
-                console.log(result);
-                if (result) {
-                    return Session.set('result', result);
+            method: function (error, answer) {
+                console.log('asdasdadad');
+                if (answer) {
+                    Session.set('answer', answer);
                 }
             }
         }
--- common/common.js
+++ common/common.js
@@ -1,12 +1,175 @@
 Schema = {};
-Schema.quizSchema = new SimpleSchema({
+Schema.baseQuestion = new SimpleSchema({
+    answer: {
+        type: String,
+        label: "Ar esate e-taškų kaupimo dalyvis?",
+        autoform: {
+            type: "select-radio",
+            options: function () {
+                return [
+                    {
+                        label: "Taip",
+                        value: 1
+                    },
+                    {
+                        label: "Ne",
+                        value: 0
+                    }
+                ];
+            }
+        }
+    },
+
+    questionNr: {
+        type: String,
+        optional: true,
+        autoform: {
+            afFieldInput: {
+                type: "hidden",
+                value: "0"
+            }
+        }
+    },
+    question: {
+        type: String,
+        optional: true,
+        autoform: {
+            afFieldInput: {
+                type: "hidden",
+                value: "Ar esate e-taškų kaupimo dalyvis?"
+            }
+        }
+    }
+});
+
+Schema.question1 = new SimpleSchema({
     answer: {
         type: String,
         label: "qqqq",
         autoform: {
             type: "select-radio",
             group: "qqqq",
-            options: function() {
+            options: function () {
+                return [{
+                    label: "Yews",
+                    value: 1
+                }, {
+                    label: "Noa",
+                    value: 0
+                }];
+            }
+        }
+    },
+
+    questionNr: {
+        type: String,
+        optional: true,
+        autoform: {
+            afFieldInput: {
+                type: "hidden",
+                value: "1"
+            }
+        }
+    },
+    question: {
+        type: String,
+        optional: true,
+        autoform: {
+            afFieldInput: {
+                type: "hidden",
+                value: "wwww"
+            }
+        }
+    }
+});
+Schema.question2 = new SimpleSchema({
+    answer: {
+        type: String,
+        label: "wwww",
+        autoform: {
+            type: "select-radio",
+            group: "wwww",
+            options: function () {
+                return [{
+                    label: "Yezs",
+                    value: 1
+                }, {
+                    label: "Noaa",
+                    value: 0
+                }];
+            }
+        }
+    },
+
+    questionNr: {
+        type: String,
+        optional: true,
+        autoform: {
+            afFieldInput: {
+                type: "hidden",
+                value: "1"
+            }
+        }
+    },
+    question: {
+        type: String,
+        optional: true,
+        autoform: {
+            afFieldInput: {
+                type: "hidden",
+                value: "qqqq"
+            }
+        }
+    }
+});
+Schema.question3 = new SimpleSchema({
+    answer: {
+        type: String,
+        label: "asasas",
+        autoform: {
+            type: "select-radio",
+            group: "asasas",
+            options: function () {
+                return [{
+                    label: "Yesqq",
+                    value: 1
+                }, {
+                    label: "Noz",
+                    value: 0
+                }];
+            }
+        }
+    },
+
+    questionNr: {
+        type: String,
+        optional: true,
+        autoform: {
+            afFieldInput: {
+                type: "hidden",
+                value: "1"
+            }
+        }
+    },
+    question: {
+        type: String,
+        optional: true,
+        autoform: {
+            afFieldInput: {
+                type: "hidden",
+                value: "qqqq"
+            }
+        }
+    }
+});
+Schema.question4 = new SimpleSchema({
+    answer: {
+        type: String,
+        label: "qqqq",
+        autoform: {
+            type: "select-radio",
+            group: "qqqq",
+            options: function () {
                 return [{
                     label: "Yes",
                     value: 1
@@ -39,5 +202,46 @@ Schema.quizSchema = new SimpleSchema({
         }
     }
 });
+Schema.question5 = new SimpleSchema({
+    answer: {
+        type: String,
+        label: "qqqq",
+        autoform: {
+            type: "select-radio",
+            group: "qqqq",
+            options: function () {
+                return [{
+                    label: "Yes",
+                    value: 1
+                }, {
+                    label: "No",
+                    value: 0
+                }];
+            }
+        }
+    },
+
+    questionNr: {
+        type: String,
+        optional: true,
+        autoform: {
+            afFieldInput: {
+                type: "hidden",
+                value: "1"
+            }
+        }
+    },
+    question: {
+        type: String,
+        optional: true,
+        autoform: {
+            afFieldInput: {
+                type: "hidden",
+                value: "qqqq"
+            }
+        }
+    }
+});
+
 
 //https://github.com/forwarder/meteor-wizard
